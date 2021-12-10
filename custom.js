760bcbea4ed7d79fcef02d7e814d665ca39b77cf
akshaykrishnam80 <88880305+akshaykrishnam80@users.noreply.github.com>
2021-12-10 15:14:56 +0000

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,28 +1,127 @@
+
+const pathname = location.pathname;
+
+var is_current = JSON.parse(localStorage.getItem("currentUser"));
+
+
 function add_user() {
   var get_users = localStorage.getItem("users");
+  var is_get_user = (get_users ? JSON.parse(get_users) : []);
 
-  var data = [];
+  console.log(is_get_user)
 
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
 
-  var bucket = {
-    email: email,
-    password: password,
-  };
+  /* letting user  */
 
-  data.push(bucket);
+  if (myInput.value.length >= 8 && myInput.value.length <= 12) {
 
-  localStorage.setItem("users", JSON.stringify(data));
+    var bucket = {
+      email: email,
+      password: password,
+    };
+
+
+    var merge_data = [...is_get_user, bucket];
+
+
+
+    localStorage.setItem("users", JSON.stringify(merge_data));
+
+    document.getElementById("outcome").innerHTML = "<b>You have Registered Successfully</b>";
+    //If user successfully registered than redirect to the login page
+    setTimeout(function () { window.location.href = 'http://localhost/gameapp/login' }, 2000)
+  }
 
 }
 
+if (pathname.includes("gameapp/login") || pathname.includes("gameapp/signup")) {
+
+  var myInput = document.getElementById("password");
+
+  var length = document.getElementById("length");
+
+  // if the user click password it will show
+  myInput.onfocus = function () {
+    document.getElementById("message").style.display = "block";
+  }
+
+  // When the user clicks outside from password box it will be close and hidden
+  myInput.onblur = function () {
+    document.getElementById("message").style.display = "none";
+  }
+
+  // if the user going to write inside the box these code will validate with inputs.
+  myInput.onkeyup = function () {
+
+    // Validate size of password
+    if (myInput.value.length >= 8 && myInput.value.length <= 12) {
+      length.classList.remove("wrong");
+      length.classList.add("right");
+      document.getElementById('signupBtn').disabled = false;
+
+    }
+
+    else {
+      length.classList.remove("right");
+      length.classList.add("wrong");
+      document.getElementById('signupBtn').disabled = true;
+
+    }
+  }
+
+}
+ //This is a redirect functionality
+
+if (is_current == null && pathname.includes("gameapp/home") ) {
+  window.location.href = 'http://localhost/gameapp/signup';
+}
+
+//This is a login functionlity
 function login_user() {
 
   var data = [];
 
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
+
+  var users_data = JSON.parse(localStorage.getItem('users'));
+  var resp = users_data.find(params => params.email == email && params.password == password);
+
+
+  if (email == resp.email && password == resp.password) {
+    localStorage.setItem("currentUser", JSON.stringify(resp));
+
+
+    setTimeout(function () { window.location.href = 'http://localhost/gameapp/home' }, 3000)
+
+
+
+  }
+
+
+}
+
+
+
+
+if (is_current != null) {
+  document.getElementById("logout-nav").classList.add('d-block');
+  document.getElementById("login-nav").classList.add('d-none');
+
+} else {
+  document.getElementById("logout-nav").classList.add('d-none');
+  document.getElementById("login-nav").classList.add('d-block');
+
+}
+
+//Logout Function
+function logout() {
+  var myItem = localStorage.getItem('users');
+  localStorage.clear();
+  localStorage.setItem('users', myItem);
+  setTimeout(function () { window.location.href = 'http://localhost/gameapp/home' }, 3000)
 
 }
--- home.php
+++ home.php
@@ -2,7 +2,156 @@
 
 
 $html = '';
-$html .= '<h1 class="home-head" >Users can play my game in here.</h1>';
+$html .= '<div class="container">
+<div class="road">
+<span id="time">05:00</span> 
+</div>
+
+
+</div>
+
+
+<script>
+ 
+/* Here explained about the timer when the game start */
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector("#time");
+    startTimer(fiveMinutes, display);
+};
+
+        const roadplace = document.querySelector(".road");
+        let driver = { step: 5 };
+        let keys = { ArrowUp: false, ArrowDown: false, ArrowLeft: false, ArrowRight: false };
+        document.addEventListener("keydown", keyDown);
+        document.addEventListener("keyup", keyUp);
+
+        function keyDown(ev) {
+            keys[ev.key] = true;
+
+        }
+        function keyUp(ev) {
+            keys[ev.key] = false;
+        }
+        /* Here i used moveline function for moving the line inside the road */
+        function movelines() {
+            let road_lines = document.querySelectorAll(".line");
+            road_lines.forEach(function (item) {
+                if (item.y >= 700) {
+                    item.y = item.y - 750;
+                }
+                item.y = item.y + driver.step;
+                item.style.top = item.y + "px";
+            })
+        }
+        function init() {
+            driver.start = true;
+            window.requestAnimationFrame(playarea);
+
+            let my_car = document.createElement("div");
+            my_car.setAttribute("class", "car");
+            roadplace.appendChild(my_car);
+
+            driver.x = my_car.offsetLeft;
+            driver.y = my_car.offsetTop;
+
+            for (x = 0; x < 5; x++) {
+
+                let road_lines = document.createElement("div");
+                road_lines.setAttribute("class", "line");
+                road_lines.y = x * 150;
+                road_lines.style.top = road_lines.y + "px";
+                roadplace.appendChild(road_lines);
+
+            }
+            for (x = 0; x < 4; x++) {
+                let motor_vehicle = document.createElement("div");
+                motor_vehicle.setAttribute("class", "vehicle");
+                motor_vehicle.y = ((x + 1) * 600) * -4;
+                motor_vehicle.style.top = motor_vehicle.y + "px";
+
+
+                motor_vehicle.style.left = Math.floor(Math.random() * 250) + "px";
+                roadplace.appendChild(motor_vehicle);
+            }
+        }
+        /* Iam using player area function for controlling the my car inside the boundary */
+        function playarea() {
+            let my_car = document.querySelector(".car");
+            let road = roadplace.getBoundingClientRect();
+            if (driver.start) {
+                movelines();
+                movingvehicles(my_car);
+                if (keys.ArrowUp && driver.y > (road.top + 100)) {
+                    driver.y = driver.y - driver.step;
+                }
+
+                if (keys.ArrowDown && driver.y < (road.bottom - 120)) {
+                    driver.y = driver.y + driver.step;
+                }
+                if (keys.ArrowLeft && driver.x > 0) {
+                    driver.x = driver.x - driver.step;
+                }
+                if (keys.ArrowRight && driver.x < (road.width - 64)) {
+                    driver.x = driver.x + driver.step;
+                }
+
+                my_car.style.top = driver.y + "px";
+                my_car.style.left = driver.x + "px";
+                window.requestAnimationFrame(playarea);
+            }
+        }
+        /* Iam using moving vehicle function for seting the vehicle in the correct path. */
+        function movingvehicles(my_car) {
+            let motor_vehicle = document.querySelectorAll(".vehicle");
+            mycarboun = my_car.getBoundingClientRect();
+
+            motor_vehicle.forEach(function (item) {
+                othercarboun = item.getBoundingClientRect();
+
+
+
+                if (!((mycarboun.bottom < othercarboun.top) ||
+                    (mycarboun.top > othercarboun.bottom) ||
+                    (mycarboun.left > othercarboun.right) ||
+                    (mycarboun.right < othercarboun.left))) {
+
+                    driver.start = false;
+                }
+                if (item.y > 500) {
+                    item.y = -200;
+                    item.style.left = Math.floor(Math.random() * 350) + "px";
+                }
+                item.y = item.y + driver.step;
+                item.style.top = item.y + "px";
+            })
+        }
+
+
+
+        init();
+
+
+
+
+
+</script>';
 
 echo $html;
 
